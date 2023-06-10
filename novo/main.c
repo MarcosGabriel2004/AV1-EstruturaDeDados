617d4a4aa7125da7ab59883d7b3da84d9862587e
danielvramos <vieira.ramos@gmail.com>
2023-06-10 01:13:25 -0300

    Totdas as funcionalidade do menu produto testadas

--- novo/main.c
+++ novo/main.c
@@ -10,22 +10,29 @@ typedef struct produto {
         int qtd_estoque;
         float preco;
 } Produto, *PProduto,**PPProduto;
-
-void incluir_produto_estoque(PPProduto* lista,int* qtd_itens_estoque){
-   int novo_indice = -1;
+void limparBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+        // Descartar caractere
+    }
+}
+void incluir_produto_estoque(PPProduto* lista,int* indice_ult_produto){
+   int novo_codigo = -1;
    //int ultimo_indice = -1;
    //printf("--->tamanho (%d)",sizeof(*lista));
-   if (*qtd_itens_estoque == 0){
-            novo_indice = 1;
+   if (*indice_ult_produto < 0){
+            novo_codigo = 1;
             *lista = (PPProduto) malloc(sizeof(PProduto)); 
              
             } 
    else{
-        novo_indice = *qtd_itens_estoque+1;
+    
+        novo_codigo = (*lista)[*indice_ult_produto]->codigo + 1;
        //ultimo_indice = (*lista)[*qtd_itens_estoque]->codigo + 1;
       // ultimo_indice
        //*qtd_itens_estoque += 1;
-       *lista = (PPProduto) realloc(*lista, novo_indice*sizeof(PProduto));
+       printf("Novo tamanho %d",(*indice_ult_produto+2)*sizeof(PProduto));
+       *lista = (PPProduto) realloc(*lista, (*indice_ult_produto+2)*sizeof(PProduto));
         }
        
         
@@ -42,8 +49,8 @@ void incluir_produto_estoque(PPProduto* lista,int* qtd_itens_estoque){
 	
    
    
-   printf("Cadastrando produto cod(%d)\n\n",novo_indice);
-   produto->codigo = novo_indice;
+   printf("Cadastrando produto cod(%d)\n\n",novo_codigo);
+   produto->codigo = novo_codigo;
    printf("Informe a Descricao do Produto: "); 
    scanf(" %[^\n]",produto->descricao);
    printf("Informe a Quantidade em estoque: "); 
@@ -51,9 +58,9 @@ void incluir_produto_estoque(PPProduto* lista,int* qtd_itens_estoque){
    printf("Informe a Valor do produto: "); 
    scanf("%f",&(produto->preco));
    
-   
-  (*lista)[*qtd_itens_estoque] = produto;  
-   *qtd_itens_estoque = novo_indice;
+   *indice_ult_produto = *indice_ult_produto + 1;
+  (*lista)[*indice_ult_produto] = produto;  
+                 
    
    
    printf("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"); 
@@ -63,18 +70,20 @@ void incluir_produto_estoque(PPProduto* lista,int* qtd_itens_estoque){
    printf("\n\nIncluir novo Produto?\nDigite: S -> ");
    char again = 'n';
    scanf(" %c",&again);
-  
+   limparBuffer();
+
    if (again == 'S' || again == 's' ){
-      incluir_produto_estoque(lista,qtd_itens_estoque);
+ 
+      incluir_produto_estoque(lista,indice_ult_produto);
    } 	
                       
-            
+  
   
 }
 
-int codigo_existe(PPProduto lista,int qtd_itens_estoque, int codigo_para_verificar){
+int codigo_existe(PPProduto lista,int indice_ult_produto, int codigo_para_verificar){
     int i;
-    for(i=0; i < qtd_itens_estoque; i++){
+    for(i=0; i <= indice_ult_produto; i++){
              if(lista[i]->codigo == codigo_para_verificar){
                                  return 1;
                                  }
@@ -84,16 +93,11 @@ int codigo_existe(PPProduto lista,int qtd_itens_estoque, int codigo_para_verific
     printf("\n ---------------------------------------------");
     return 0;
     }
-void limparBuffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF) {
-        // Descartar caractere
-    }
-}
-void alterar_item(PPProduto* lista,int qtd_itens_estoque,int cod_item){
+
+void alterar_item(PPProduto* lista,int indice_ult_produto,int cod_item){
    int i;
    PProduto produto;  
-   for(i=0;i<qtd_itens_estoque; i++){
+   for(i=0;i<=indice_ult_produto; i++){
                                 if ((*lista)[i]->codigo == cod_item){
                                 produto = (*lista)[i];
                                 }
@@ -117,10 +121,11 @@ void alterar_item(PPProduto* lista,int qtd_itens_estoque,int cod_item){
    printf("\n Produto alterado com sucesso!!!");
    printf("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");   
      }
-void consultar_item(PPProduto lista,int qtd_itens_estoque,int cod_item){
+void consultar_item(PPProduto lista,int indice_ult_produto,int cod_item){
    int i;
    PProduto produto;  
-   for(i=0;i<qtd_itens_estoque; i++){
+   
+   for(i=0;i<=indice_ult_produto; i++){
                                 if (lista[i]->codigo == cod_item){
                                 produto = lista[i];
                                 }
@@ -135,8 +140,8 @@ void consultar_item(PPProduto lista,int qtd_itens_estoque,int cod_item){
    
      }
 
-int menu_escolha_item(PPProduto lista,int qtd_itens_estoque, char msgm[]){
-     if(qtd_itens_estoque == 0 ){
+int menu_escolha_item(PPProduto lista,int indice_ult_produto, char msgm[]){
+     if(indice_ult_produto < 0 ){
      return 0;
      }
      int cod = 0;
@@ -145,14 +150,14 @@ int menu_escolha_item(PPProduto lista,int qtd_itens_estoque, char msgm[]){
         printf(msgm);
         scanf("%d",&cod);
         limparBuffer();          
-     }while(!codigo_existe(lista,qtd_itens_estoque,cod));
+     }while(!codigo_existe(lista,indice_ult_produto,cod));
      return cod;
 }
 
-void exibir_produtos_estoque(PPProduto lista,int qtd_itens_estoque){
+void exibir_produtos_estoque(PPProduto lista,int indice_ult_produto){
      printf("\n\n");
      
-     if(qtd_itens_estoque == 0){
+     if(indice_ult_produto < 0){
              printf("\n ---------------------------------------------");
              printf("\nNENHUM ITEM NO ESTOQUE CADASTRE ALGUM ITEM.");
              printf("\n ---------------------------------------------");
@@ -161,7 +166,7 @@ void exibir_produtos_estoque(PPProduto lista,int qtd_itens_estoque){
      float total_geral = 0.0;
      int i;
      
-     for(i=0; i < qtd_itens_estoque; i++){
+     for(i=0; i <= indice_ult_produto; i++){
              PProduto prod = lista[i];
              float total_prod = prod->qtd_estoque * prod->preco;
              total_geral += total_prod;
@@ -171,6 +176,32 @@ void exibir_produtos_estoque(PPProduto lista,int qtd_itens_estoque){
           
      //printf("\n\n VALOR TOTAL DO ESTOQUE R$ %.2f EM %d ITENS\n",total_geral,qtd_itens_estoque);
      
+     }
+void excluir_item(PPProduto* lista,int* indice_ult_produto, int cod_excluir){
+     int i,j;
+       PProduto produto_para_excluir;  
+       for(i=0;i<=*indice_ult_produto; i++){
+                                    if ((*lista)[i]->codigo == cod_excluir){
+                                        produto_para_excluir = (*lista)[i];
+                                        for (j=i+1;j<=*indice_ult_produto; j++){
+                                            (*lista)[i] = (*lista)[j];
+                                            i++;
+                                            }
+                                    }
+       }
+       (*lista)[*indice_ult_produto] = produto_para_excluir;
+       if(*indice_ult_produto == 0){
+        free((*lista));
+       }else{
+           *lista = (PPProduto) realloc(*lista,*indice_ult_produto*sizeof(PProduto));  
+       }
+       *indice_ult_produto -=1;
+   printf("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"); 
+   printf("\n Produto excluido com sucesso!!!");
+   printf("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"); 
+   printf("\n\nProduto cod.'%d' - ",cod_excluir);
+   printf("%s\n",produto_para_excluir->descricao);
+
      }
 
 int menu_principal_opcoes(){
@@ -223,7 +254,7 @@ int menu_gerenciar_pedido_opcoes(){
 	return opcao;
 }
 
-void gerenciar_menu_produto(PPProduto* estoque,int* qtd_itens_estoque){
+void gerenciar_menu_produto(PPProduto* estoque,int* indice_ult_produto){
 	int opcao;
 	int sair = 0;
 	do{	
@@ -234,7 +265,7 @@ void gerenciar_menu_produto(PPProduto* estoque,int* qtd_itens_estoque){
                 printf("\n######################");
 				printf("\nIncluindo Produto\n\n");
 				printf("\n######################\n");
-                incluir_produto_estoque(estoque,qtd_itens_estoque);
+                incluir_produto_estoque(estoque,indice_ult_produto);
                 sair = 0;  
 				//getch();
 				break;
@@ -243,14 +274,14 @@ void gerenciar_menu_produto(PPProduto* estoque,int* qtd_itens_estoque){
                 printf("\n#################################");
 				printf("\nEscolhendo Produto para alterar");
 				printf("\n#################################\n");
-				exibir_produtos_estoque(*estoque,*qtd_itens_estoque);
-				int cod_alterar = menu_escolha_item(*estoque,*qtd_itens_estoque,"DIGITAR CODIGO QUE DESEJA ALTERAR: ");
+				exibir_produtos_estoque(*estoque,*indice_ult_produto);
+				int cod_alterar = menu_escolha_item(*estoque,*indice_ult_produto,"DIGITAR CODIGO QUE DESEJA ALTERAR: ");
 				if (cod_alterar){ 
     				system("CLS");
                     printf("\n###################################");
     				printf("\nRealizando a alteracao do Produto");
     				printf("\n###################################\n");
-    				alterar_item(estoque,*qtd_itens_estoque,cod_alterar);
+    				alterar_item(estoque,*indice_ult_produto,cod_alterar);
                 }
 				sair = 0;
 				getch();
@@ -260,7 +291,7 @@ void gerenciar_menu_produto(PPProduto* estoque,int* qtd_itens_estoque){
                 printf("\n##################################");
 				printf("\nRelacao de produtos no estoque");
 				printf("\n#################################\n");
-				exibir_produtos_estoque(*estoque,*qtd_itens_estoque);
+				exibir_produtos_estoque(*estoque,*indice_ult_produto);
                 sair = 0;
                 getch();
 				break;
@@ -270,15 +301,15 @@ void gerenciar_menu_produto(PPProduto* estoque,int* qtd_itens_estoque){
 				printf("\nConsultar Produto");
 				printf("\n######################\n");
 				
-				if(*qtd_itens_estoque == 0){
+				if(*indice_ult_produto < 0){
                  printf("\n ---------------------------------------------");
                  printf("\nNENHUM ITEM NO ESTOQUE CADASTRE ALGUM ITEM.");
                  printf("\n ---------------------------------------------");
                 }else{
-                int cod_consultar = menu_escolha_item(*estoque,*qtd_itens_estoque,"DIGITAR CODIGO QUE DESEJA CONSULTAR: ");
+                int cod_consultar = menu_escolha_item(*estoque,*indice_ult_produto,"DIGITAR CODIGO QUE DESEJA CONSULTAR: ");
                     if (cod_consultar){
                        system("CLS");
-                       consultar_item(*estoque,*qtd_itens_estoque,cod_alterar);
+                       consultar_item(*estoque,*indice_ult_produto,cod_consultar);
                     }
                 }
                 getch();
@@ -288,11 +319,15 @@ void gerenciar_menu_produto(PPProduto* estoque,int* qtd_itens_estoque){
                 printf("\n#################################");
 				printf("\nEscolhendo Produto para Excluir");
 				printf("\n#################################\n");
-				exibir_produtos_estoque(*estoque,*qtd_itens_estoque);
-				int cod_excluir = menu_escolha_item(*estoque,*qtd_itens_estoque,"DIGITAR CODIGO QUE DESEJA CONSULTAR: ");
+				exibir_produtos_estoque(*estoque,*indice_ult_produto);
+				int cod_excluir = menu_escolha_item(*estoque,*indice_ult_produto,"DIGITAR CODIGO QUE DESEJA EXCLUIR: ");
                 if (cod_excluir){
                    system("CLS");
-                   //excluir_item(estoque,qtd_itens_estoque,cod_excluir);
+                   printf("\n#################################");
+    			   printf("\nExcluindo Produto cod:'%d'",cod_excluir);
+    			   printf("\n#################################\n");
+                   
+                   excluir_item(estoque,indice_ult_produto,cod_excluir);
                 }
                 sair = 0;
 				getch();
@@ -341,7 +376,7 @@ void gerenciar_menu_pedido(){
 				getch();
 				break;								
 			case 7:
-				printf("Voltar");
+				//printf("Voltar");
 				sair = 1;
 				break;
 			default:
@@ -353,15 +388,15 @@ void gerenciar_menu_pedido(){
 	
 }
 
-void gerenciar_menu_principal(PPProduto* estoque,int* qtd_itens_estoque){
+void gerenciar_menu_principal(PPProduto* estoque,int* indice_ult_produto){
 	int opcao;
 	int sair = 0;
 	do{	
 		opcao = menu_principal_opcoes();		
 		switch(opcao){
 			case 1:				
-				gerenciar_menu_produto(estoque,qtd_itens_estoque);
-				getch();
+				gerenciar_menu_produto(estoque,indice_ult_produto);
+				//getch();
 				break;
 			case 2:			
 				gerenciar_menu_pedido();
@@ -382,7 +417,7 @@ void gerenciar_menu_principal(PPProduto* estoque,int* qtd_itens_estoque){
 
 int main(int argc, char *argv[]) {
     PPProduto estoque;
-    int qtd_itens_estoque = 0;
-	gerenciar_menu_principal(&estoque,&qtd_itens_estoque);	
+    int indice_ult_produto = -1;
+	gerenciar_menu_principal(&estoque,&indice_ult_produto);	
 	return 0;
 }
